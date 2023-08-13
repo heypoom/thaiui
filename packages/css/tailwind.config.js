1538e30a880b85c7086c4468e4336339321b6792
Phoomparin Mano <phoomparin@gmail.com>
2023-08-13 23:12:34 +0700

    feat: basic components

--- packages/css/tailwind.config.js
+++ packages/css/tailwind.config.js
@@ -2,11 +2,20 @@
 module.exports = {
   content: [],
   theme: {
+    screens: {
+      sm: '576px',
+      md: '768px',
+      lg: '992px',
+      xl: '1200px',
+      xxl: '1400px',
+    },
     extend: {
       colors: {
-        primary: 'var(--color-primary)',
+        primary: 'var(--th-primary)',
+        secondary: 'var(--th-secondary)',
       },
     },
   },
+
   plugins: [],
 }
